5c7b6206d91aaf53e1e368f8f96e81c792bc8b17
vktskm <vinkentskembri@gmail.com>
2023-04-03 16:20:13 +0200

    primo javascript

---
+++ m1/s2/g1/js/naming.js
@@ -0,0 +1,18 @@
+//var 1 pet_preferito = 'gatto';  dichiarazione sbagliata di variabile
+//console.log(1 pet_preferito);  non funziona perchè la variabile è dichiarata sbagliata
+
+/*  var pet = 'gatto'; convenzione giusta token
+    var Pet = 'cane';    convezione sbagliata token*/ 
+
+document.write("<b>Eseguo elemento script di naming.js</b><hr>");
+
+//stampa in console giraffa , convenzione sbagliata token
+var $pet = 'giraffa';
+console.log($pet);
+//stampa in console leone , convenzione sbagliata token
+var _pet = 'leone';
+console.log(_pet);
+//mi stampa pantera nel HTML ma il metodo write da problemi si vede in console
+var _pet2 = 'pantera';
+document.write(_pet2);
+
