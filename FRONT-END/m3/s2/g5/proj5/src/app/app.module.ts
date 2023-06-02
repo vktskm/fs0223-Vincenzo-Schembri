0922e7992ae35efb77c8bfcec364bfdd2ff7fbba
vktskm <vinkentskembri@gmail.com>
2023-06-02 10:39:37 +0200

    kkkkk

---
+++ FRONT-END/m3/s2/g5/proj5/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './Pages/todos/todos.component';
+import { CompletatiComponent } from './Pages/completati/completati.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { NavbarComponent } from './Pages/navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    CompletatiComponent,
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
